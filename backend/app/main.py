89587f39b9cc0d97309473601985249b77a02340
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2023-04-01 14:04:10 +0700

    update message publish method

--- backend/app/main.py
+++ backend/app/main.py
@@ -68,7 +68,8 @@ async def send_message(room_id: int, content: str, db: Session = Depends(get_db)
     channel = await app.state.rabbitmq_connection.channel()
 
     message_data = {"room_id": room_id, "content": content}
-    await channel.basic_publish(body=Message(json.dumps(message_data).encode()), routing_key=f"room_{room_id}_queue")
+    message = Message(json.dumps(message_data).encode())
+    await channel.default_exchange.publish(body=message, routing_key=f"room_{room_id}_queue")
     # queue = await channel.declare_queue(f"room_{room_id}_queue")
     # await queue.publish(Message(json.dumps(message_data).encode()))
 
