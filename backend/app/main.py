3748e94ee6431e0643ddd929eb301d868e7c0e98
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2023-04-01 13:43:39 +0700

    revert dependency to get_db

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,13 +2,14 @@ import json
 
 from dotenv import load_dotenv
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
 
 from aio_pika import connect, Message
 
 from . import crud, models, schemas
-from .database import SessionManager, engine
+from .database import SessionLocal, engine
 
 load_dotenv()
 models.Base.metadata.create_all(bind=engine)
@@ -18,6 +19,14 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
 )
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 async def get_rabbitmq_connection():
@@ -40,25 +49,22 @@ async def get_rooms():
 
 
 @app.get("/rooms", response_model=list[schemas.Room])
-async def get_rooms():
-    with SessionManager() as db:
-        return crud.get_rooms(db)
+async def get_rooms(db: Session = Depends(get_db)):
+    return crud.get_rooms(db)
 
 
 @app.post("/rooms", response_model=schemas.Room)
-async def create_room():
-    with SessionManager() as db:
-        return crud.create_room(db)
+async def create_rooms(db: Session = Depends(get_db)):
+    return crud.create_room(db)
 
 
 @app.get("/rooms/{room_id}/messages", response_model=list[schemas.Message])
-async def get_messages(room_id: int):
-    with SessionManager() as db:
-        return crud.get_messages(db, room_id)
+async def get_messages(room_id: int, db: Session = Depends(get_db)):
+    return crud.get_messages(db, room_id)
 
 
 @app.post("/rooms/{room_id}/messages")
-async def send_message(room_id: int, content: str):
+async def send_message(room_id: int, content: str, db: Session = Depends(get_db)):
     channel = await app.state.rabbitmq_connection.channel()
     queue = await channel.declare_queue(f"room_{room_id}_queue")
 
